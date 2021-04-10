dd1837e9e01192e019c5f8f80d1bcedb0708a27b
blightn <blightan@gmail.com>
2021-04-10 21:22:06 +0300

    first

---
+++ asynctest/urls.py
@@ -0,0 +1,28 @@
+"""asynctest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from asynctest.views import index, download1, download2, download3, download4
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", index),
+    path("download1/", download1),
+    path("download2/", download2),
+    path("download3/", download3),
+    path("download4/", download4),
+]
---
+++ asynctest/views.py
@@ -0,0 +1,124 @@
+from django.http import HttpResponse
+
+import asyncio
+import os
+from django.conf import settings
+
+import requests
+from asgiref.sync import sync_to_async
+
+import aiohttp
+import aiofiles
+
+
+
+files_for_downloading = (
+    'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt', # 4.04 MB - 4.2 MB (4,234,901 bytes)
+    'https://raw.githubusercontent.com/zacanger/profane-words/master/words.json',  # 37.6 KB - 38.5 kB (38,477 bytes)
+    'https://raw.githubusercontent.com/filiph/english_words/master/pubspec.yaml',  # 413 Bytes - 413 bytes
+)
+
+download_path = os.path.join(settings.BASE_DIR, 'downloads')
+
+
+async def index(request):
+    return HttpResponse(
+        '<h1 align="center">Downloader Pro</h1>'
+        '<br>'
+        '<p align="center"><a href="download1/">Download (requests)</a></p>'
+        '<p align="center"><a href="download2/">Download (asyncio)</a></p>'
+        '<p align="center"><a href="download3/">Download (gather)</a></p>'
+        '<p align="center"><a href="download4/">Download (as_completed)</a></p>'
+        )
+
+
+async def download_async1(file_url):
+    r = await sync_to_async(requests.get, thread_sensitive=True)(file_url, allow_redirects=True)
+
+    if r.status_code != requests.codes.ok:
+        raise
+
+    file_name = file_url.split('/')[-1]
+    file_path = os.path.join(download_path, file_name)
+
+    f = open(file_path, 'wb')
+    await sync_to_async(f.write, thread_sensitive=True)(r.content)
+
+    print(f'File {file_name} was downloaded to {file_path}')
+
+    return file_name
+
+
+async def download_async2(file_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(file_url) as r:
+            if r.status != 200:
+                raise
+
+            data = await r.read()
+
+    file_name = file_url.split('/')[-1]
+    file_path = os.path.join(download_path, file_name)
+
+    async with aiofiles.open(file_path, 'wb') as f:
+        await f.write(data)
+
+    print(f'File {file_name} was downloaded to {file_path}')
+
+    return file_name
+
+
+# requests
+# Ждёт завершения.
+async def download1(request):
+    '''
+    loop = asyncio.get_event_loop()
+
+    for url in files_for_downloading:
+        loop.create_task(download_async1(url))
+    '''
+
+    for url in files_for_downloading:
+        asyncio.create_task(download_async1(url))
+
+    return HttpResponse('<h1 align="center">Downloaded</h1>')
+
+
+# asyncio
+# Не ждёт завершения.
+async def download2(request):
+    '''
+    loop = asyncio.get_event_loop()
+
+    for url in files_for_downloading:
+        loop.create_task(download_async2(url))
+    '''
+
+    for url in files_for_downloading:
+        asyncio.create_task(download_async2(url))
+
+    return HttpResponse('<h1 align="center">Downloading...</h1>')
+
+
+# gather
+# Ждёт завершения.
+async def download3(request):
+    coros = [download_async2(url) for url in files_for_downloading]
+    await asyncio.gather(*coros)
+
+    return HttpResponse('<h1 align="center">Downloaded</h1>')
+
+
+# as_completed
+# Ждёт завершения.
+async def download4(request):
+    coros = [download_async2(url) for url in files_for_downloading]
+    
+    for future in asyncio.as_completed(coros):
+        try:
+            file_name = await future
+            print(f'File {file_name} downloaded')
+        except Exception:
+            print(f'Error while downloading file {file_name}')
+
+    return HttpResponse('<h1 align="center">Downloaded</h1>')
